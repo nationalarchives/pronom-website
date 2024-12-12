79d5de280878ca36c15ab2be28b07174490712fe
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2024-12-12 12:27:10 +0000

    Move create function out of the loop

--- .github/scripts/generate_index_file.py
+++ .github/scripts/generate_index_file.py
@@ -40,21 +40,26 @@ def insert_into_indexes(path_value: str, field_name: str, field_value: str):
         conn.close()
 
 
-for file_path in json_files:
+def run():
     create_table()
-    with open(f'{path}/signatures/{file_path}', 'r') as file:
-        data_values = []
-        data = json.load(file)
-        format_name = data['formatName']
-        puid = [idf['identifierText'] for idf in data['identifiers'] if idf['identifierType'] == 'PUID'][0]
-        for value in data.values():
-            if type(value) is list:
-                for each in value:
-                    for obj_value in each.values():
-                        if obj_value is not None:
-                            data_values.append(str(obj_value))
-            else:
-                if value is not None:
-                    data_values.append(str(value))
-        field = ''.join([str(field) for field in data_values])
-        insert_into_indexes(puid, format_name, field)
+    for file_path in json_files:
+
+        with open(f'{path}/signatures/{file_path}', 'r') as file:
+            data_values = []
+            data = json.load(file)
+            format_name = data['formatName']
+            puid = [idf['identifierText'] for idf in data['identifiers'] if idf['identifierType'] == 'PUID'][0]
+            for value in data.values():
+                if type(value) is list:
+                    for each in value:
+                        for obj_value in each.values():
+                            if obj_value is not None:
+                                data_values.append(str(obj_value))
+                else:
+                    if value is not None:
+                        data_values.append(str(value))
+            field = ''.join([str(field) for field in data_values])
+            insert_into_indexes(puid, format_name, field)
+
+
+run()
