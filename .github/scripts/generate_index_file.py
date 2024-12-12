41e9b1e0c354831b130982081a1ac0cc1b9943e4
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2024-12-12 12:23:01 +0000

    Insert more than one row

--- .github/scripts/generate_index_file.py
+++ .github/scripts/generate_index_file.py
@@ -13,12 +13,23 @@ def get_json_files(prefix):
 json_files = get_json_files('fmt') + get_json_files('x-fmt')
 
 
-def insert_into_indexes(path_value: str, field_name: str, field_value: str):
+def create_table():
     conn = sqlite3.connect("indexes")
     try:
         cursor = conn.cursor()
         cursor.execute("DROP TABLE IF EXISTS indexes")
         cursor.execute("CREATE TABLE IF NOT EXISTS indexes (path, name, field)")
+        conn.commit()
+    except sqlite3.Error as e:
+        print(f"An error occurred: {e}")
+    finally:
+        conn.close()
+
+
+def insert_into_indexes(path_value: str, field_name: str, field_value: str):
+    conn = sqlite3.connect("indexes")
+    try:
+        cursor = conn.cursor()
         cursor.execute("INSERT INTO indexes (path, name, field) VALUES (?, ?, ?)",
                        (path_value, field_name, field_value))
 
@@ -30,11 +41,12 @@ def insert_into_indexes(path_value: str, field_name: str, field_value: str):
 
 
 for file_path in json_files:
+    create_table()
     with open(f'{path}/signatures/{file_path}', 'r') as file:
         data_values = []
         data = json.load(file)
         format_name = data['formatName']
-        puid = [identifier['identifierText'] for identifier in data['identifiers'] if identifier['identifierType'] == 'PUID'][0]
+        puid = [idf['identifierText'] for idf in data['identifiers'] if idf['identifierType'] == 'PUID'][0]
         for value in data.values():
             if type(value) is list:
                 for each in value:
