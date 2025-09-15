3f0d518b58ba3bd433dfed1b6bab0a8e0eb8625c
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2025-09-15 21:44:56 +0100

    Install first

--- run-server.sh
+++ run-server.sh
@@ -2,7 +2,7 @@
 set -e
 SIGNATURE_FILES_LOCATION=$1
 mkdir -p site/fmt site/x-fmt site/actor site/edit/fmt site/edit/x-fmt site/actor/edit
-poetry add pycountry ghapi boto3
+poetry install
 python3 .github/scripts/generate_pages.py $SIGNATURE_FILES_LOCATION
 python3 .github/scripts/generate_index_file.py $SIGNATURE_FILES_LOCATION
 cd site
