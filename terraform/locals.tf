86de7e488ea97dae1220354a709b3765477765a3
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2026-04-30 09:53:12 +0100

    DR2-2813 Signature files not rendering.
    The problem is the CSP. The browser uses inline styles to format the XML
    which are blocked by the CSP.
    
    This is a slightly flaky way of doing this but we don't have a lot of
    other options. `sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=` is
    the sha of the empty string so this won't change and for the other sha,
    I found a post from 2 years ago with the exact issue and exact sha so
    this doesn't look like it changes much either.

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -9,7 +9,7 @@ locals {
 
   domain_name = "pronom.nationalarchives.gov.uk"
 
-  content_security_policy = "default-src 'self'; base-uri 'none'; object-src 'none'; font-src 'self' https://fonts.gstatic.com https://use.typekit.net; style-src 'self' https://www.nationalarchives.gov.uk https://fonts.googleapis.com https://p.typekit.net https://use.typekit.net; script-src 'self' https://www.nationalarchives.gov.uk"
+  content_security_policy = "default-src 'self'; base-uri 'none'; object-src 'none'; font-src 'self' https://fonts.gstatic.com https://use.typekit.net; style-src 'self' https://www.nationalarchives.gov.uk https://fonts.googleapis.com https://p.typekit.net https://use.typekit.net 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=' 'sha256-p08VBe6m5i8+qtXWjnH/AN3klt1l4uoOLsjNn8BjdQo='; script-src 'self' https://www.nationalarchives.gov.uk"
 
   python_runtime = "python3.13"
 
