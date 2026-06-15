2b40e2e5a934a7ddbf66b3fc291c4c55ae1127e0
davidkapcsandi <98022110+davidkapcsandi@users.noreply.github.com>
2026-06-15 14:25:57 +0100

    Updated workflow

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -19,7 +19,7 @@ aws s3 sync --content-type text/html  --exclude "*.css" --exclude "*.js" --exclu
 aws s3 cp fa-solid-900.woff2 $S3_URL
 cd ..
 
-LATEST_SIGNATURE_FILE=$(aws s3 ls "$S3_URL/signatures/" | sort -t'V' -k2,2n | tail -1 | awk '{split($0,a," "); print a[4]}')
+LATEST_SIGNATURE_FILE=$(aws s3 ls "PROD_$S3_URL/signatures/" | sort -t'V' -k2,2n | tail -1 | awk '{split($0,a," "); print a[4]}')
 docker compose exec app poetry run python .github/scripts/generate_version_file.py "$LATEST_SIGNATURE_FILE"
 docker compose cp app:/app/version .
 
@@ -36,11 +36,17 @@ cd lambdas || exit
 cd soap
 zip -rq ../../soap.zip .
 cd ../../
-aws s3 cp "$S3_URL/signatures/$LATEST_SIGNATURE_FILE" signature-file.xml
+aws s3 cp "PROD_$S3_URL/signatures/$LATEST_SIGNATURE_FILE" signature-file.xml
 zip -q soap.zip version signature-file.xml
 
 cp ./*.zip terraform
 cd terraform || exit
 terraform init
 TF_VAR_environment=$ENVIRONMENT TF_VAR_latest_signature_version=$LATEST_SIGNATURE_FILE terraform apply --auto-approve
+cd html
+aws s3 sync --content-type text/css  --exclude "*" --include "*.css" . $S3_URL
+aws s3 sync --content-type text/javascript  --exclude "*" --include "*.js" . $S3_URL
+aws s3 sync --content-type text/html  --exclude "*.css" --exclude "*.js" --exclude "fa-solid-900.woff2" . $S3_URL
+aws s3 cp fa-solid-900.woff2 $S3_URL
+cd ..
 aws cloudfront create-invalidation --distribution-id $(aws cloudfront list-distributions --query 'DistributionList.Items[0].Id' --output text) --paths "/*"
