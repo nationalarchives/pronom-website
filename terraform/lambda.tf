9fd8f66f8a6c753c1dd2ab775299c50378b0a58b
davidkapcsandi <98022110+davidkapcsandi@users.noreply.github.com>
2026-06-26 09:55:31 +0100

    DR2-2835 Removed Snapstart from PRONOM search lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -41,17 +41,6 @@ resource "aws_lambda_function" "search" {
   handler       = "results.lambda_handler"
   filename      = "${path.module}/results.zip"
   timeout       = 60
-  snap_start {
-    apply_on = "PublishedVersions"
-  }
-  publish                        = true
-  reserved_concurrent_executions = local.lambda_reserved_concurrent_executions
-  source_code_hash               = filebase64sha256("${path.module}/results.zip")
-  environment {
-    variables = {
-      DB_NAME = "indexes"
-    }
-  }
 }
 
 resource "aws_lambda_alias" "search_alias" {
