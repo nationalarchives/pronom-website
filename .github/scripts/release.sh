af2a0d6a83cc99c4dc95877d29e6b30eeb6d9f5e
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2026-07-10 16:17:14 +0100

    Copy woff from assets

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -51,9 +51,9 @@ aws s3 sync --content-type font/woff2 --exclude "*" --include "*.woff2" . $S3_UR
 aws s3 sync --content-type image/x-icon --exclude "*" --include "*.ico" . $S3_URL
 aws s3 sync --content-type image/png --exclude "*" --include "*.png" . $S3_URL
 aws s3 sync --content-type application/json --exclude "*" --include "*.json" . $S3_URL
+aws s3 cp --content-type font/woff2 assets/fa-solid-900.woff2 $S3_URL//fa-solid-900.woff2
+aws s3 cp --content-type font/woff2 assets/fa-brands-400.woff2 $S3_URL//fa-brands-400.woff2
 aws s3 sync --content-type text/html --exclude "*.css" --exclude "*.js" --exclude "*.xml" --exclude "*.woff2" --exclude "*.ico" --exclude "*.png" --exclude "*.json" . $S3_URL
-aws s3 cp fa-solid-900.woff2 $S3_URL//fa-solid-900.woff2
-aws s3 cp fa-brands-400.woff2 $S3_URL//fa-brands-400.woff2
 aws s3 cp signatures.json $S3_URL
 aws s3 mv $S3_URL/releases.html $S3_URL/releases
 aws s3 cp ../signature-file.xml $S3_URL/binary-signatures.xml
