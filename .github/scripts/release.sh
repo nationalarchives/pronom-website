54817e7a0a05ac61462ace69343e86823badd537
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-07-10 15:19:04 +0100

    Update S3 sync commands in release script to include new file types

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -47,7 +47,10 @@ cd html
 aws s3 sync --content-type text/css  --exclude "*" --include "*.css" . $S3_URL
 aws s3 sync --content-type text/javascript  --exclude "*" --include "*.js" . $S3_URL
 aws s3 sync --content-type application/xml  --exclude "*" --include "*.xml" . $S3_URL
-aws s3 sync --content-type text/html  --exclude "*.css" --exclude "*.xml" --exclude "*.js" --exclude "fa-solid-900.woff2" --exclude "fa-brands-400.woff2" . $S3_URL
+aws s3 sync --content-type font/woff2  --exclude "*" --include "*.woff2" . $S3_URL
+aws s3 sync --content-type image/x-icon  --exclude "*" --include "*.ico" . $S3_URL
+aws s3 sync --content-type image/png  --exclude "*" --include "*.png" . $S3_URL
+aws s3 sync --content-type text/html  --exclude "*.css" --exclude "*.js" --exclude "*.xml" --exclude "*.woff2" --exclude "*.ico" --exclude "*.png" . $S3_URL
 aws s3 cp fa-solid-900.woff2 $S3_URL//fa-solid-900.woff2
 aws s3 cp fa-brands-400.woff2 $S3_URL//fa-brands-400.woff2
 aws s3 cp signatures.json $S3_URL
