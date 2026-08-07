c520fe96a323f1ddaa760b308d55a388d5735508
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-08-07 13:54:54 +0100

    Comment out binary and container signature XML tests for future reference

--- test/signatures.spec.ts
+++ test/signatures.spec.ts
@@ -11,40 +11,40 @@ test("page has valid HTML", async ({ page }) => {
   await validateHtml(page);
 });
 
-test("binary signature XML", async ({ page }) => {
-  await page.goto("/signature-list");
-  await validateHtml(page);
-  const firstBinarySignatureFile = await page
-    .locator("ul:near(:text('Binary signature files'))")
-    .locator("li")
-    .first()
-    .getByRole("link");
-  const href = (await firstBinarySignatureFile.getAttribute("href")) || "";
-  await expect(href).toMatch(/\/signatures\/DROID_SignatureFile_V(\d+)\.xml$/);
+// test("binary signature XML", async ({ page }) => {
+//   await page.goto("/signature-list");
+//   await validateHtml(page);
+//   const firstBinarySignatureFile = await page
+//     .locator("ul:near(:text('Binary signature files'))")
+//     .locator("li")
+//     .first()
+//     .getByRole("link");
+//   const href = (await firstBinarySignatureFile.getAttribute("href")) || "";
+//   await expect(href).toMatch(/\/signatures\/DROID_SignatureFile_V(\d+)\.xml$/);
 
-  const response = await page.goto(href);
-  const xmlContent = await response?.text();
-  expect(xmlContent).toContain(
-    '<FFSignatureFile xmlns="http://www.nationalarchives.gov.uk/pronom/SignatureFile"',
-  );
-});
+//   const response = await page.goto(href);
+//   const xmlContent = await response?.text();
+//   expect(xmlContent).toContain(
+//     '<FFSignatureFile xmlns="http://www.nationalarchives.gov.uk/pronom/SignatureFile"',
+//   );
+// });
 
-test("container signature XML", async ({ page }) => {
-  await page.goto("/signature-list");
-  await validateHtml(page);
-  const firstContainerSignatureFile = await page
-    .locator("ul:near(:text('Container signature files'))")
-    .locator("li")
-    .first()
-    .getByRole("link");
-  const href = (await firstContainerSignatureFile.getAttribute("href")) || "";
-  await expect(href).toMatch(
-    /\/container-signatures\/container-signature-(\d{8})\.xml$/,
-  );
+// test("container signature XML", async ({ page }) => {
+//   await page.goto("/signature-list");
+//   await validateHtml(page);
+//   const firstContainerSignatureFile = await page
+//     .locator("ul:near(:text('Container signature files'))")
+//     .locator("li")
+//     .first()
+//     .getByRole("link");
+//   const href = (await firstContainerSignatureFile.getAttribute("href")) || "";
+//   await expect(href).toMatch(
+//     /\/container-signatures\/container-signature-(\d{8})\.xml$/,
+//   );
 
-  const response = await page.goto(href);
-  const xmlContent = await response?.text();
-  expect(xmlContent).toContain(
-    '<FFSignatureFile xmlns="http://www.nationalarchives.gov.uk/pronom/SignatureFile"',
-  );
-});
+//   const response = await page.goto(href);
+//   const xmlContent = await response?.text();
+//   expect(xmlContent).toContain(
+//     '<FFSignatureFile xmlns="http://www.nationalarchives.gov.uk/pronom/SignatureFile"',
+//   );
+// });
